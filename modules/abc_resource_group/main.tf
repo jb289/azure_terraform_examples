48fee2e8fbbd07649e3c8e91ffddddce1fe3a2d6
John Brewer <jbrewha@gmail.com>
2019-09-20 15:06:21 -0500

    add initial example module

---
+++ modules/abc_resource_group/main.tf
@@ -0,0 +1,11 @@
+resource "azurerm_resource_group" "rg" {
+        name = "rg-${var.sub_name}-${var.sub_loc_abbrev}-${var.system_name}"
+        location = "${var.sub_loc}"
+
+        tags = {
+         Application = "${var.tag_application}",
+         Owner = "${var.tag_owner}"
+        }
+}
+
+
